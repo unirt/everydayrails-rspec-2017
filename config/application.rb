06c4074869edab9e83ff2c7af8c864ca538a4601
unirt <lunirtc@gmail.com>
2018-11-17 01:01:49 +0900

    Finish 4

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,10 @@ module Projects
 
     config.generators do |g|
       g.test_framework :rspec,
-      fixtures: false,
       view_specs: false,
       helper_specs: false,
-      routing_specs: false
+      routing_specs: false,
+      request_specs: false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
   
   it 'is valid with a first name, last name, email, and password' do
     user = User.new(
@@ -13,18 +17,22 @@ RSpec.describe User, type: :model do
   end
 
   it 'is invalid without a first name' do
-    user = User.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it 'is invalid without a last name' do
-    user = User.new(last_name: nil)
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
-  it 'is invalid without an email address'
+  it 'is invalid without an email address' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 
   it 'is invalid without a duplicate email address' do
     User.create(
